46db3f8a80778bf17e232e1e1767e19acf24029c
Aditya <183531672+iamAdityafr@users.noreply.github.com>
2025-11-05 23:13:50 +0530

    add [auth-service] main

---
+++ go/auth-service/cmd/server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"auth-service/internal/handler"
+	"auth-service/internal/service"
+	"bck/auth/authpb"
+	"log"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	port := os.Getenv("GRPC_PORT")
+	if port == "" {
+		log.Fatal("Field not found from .env")
+	}
+
+	// grpc setup
+	authService, err := service.NewAuthService()
+	if err != nil {
+		log.Fatalf("Failed to create auth service: %v", err)
+	}
+	authHandler := handler.NewAuthHandler(authService)
+	server := grpc.NewServer(
+		grpc.Creds(insecure.NewCredentials()),
+	)
+
+	authpb.RegisterAuthServiceServer(server, authHandler)
+	reflection.Register(server)
+	lis, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalf("Failed to listen on port %s: %v", port, err)
+	}
+
+	log.Printf("AuthService server listening on port %s", port)
+	if err := server.Serve(lis); err != nil {
+		log.Fatalf("Error in serving grpc server: %v", err)
+	}
+}
