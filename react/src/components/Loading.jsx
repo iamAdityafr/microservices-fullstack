8ea33f1eaea34fa4fc027ada7f453183a2db74c3
Aditya <183531672+iamAdityafr@users.noreply.github.com>
2025-11-17 21:48:44 +0530

    add component header loading Privateroute

---
+++ react/src/components/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center h-64">
+      <div className="w-12 h-12 border-4 border-pink-400 border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loading;
